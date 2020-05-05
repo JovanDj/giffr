ed7b381fdaf76fc980dece088b6dd9d0c46eeb5c
Jovan <jovandjukic2@gmail.com>
2020-05-05 17:16:29 +0200

    Updates

---
+++ precache-manifest.f069e5fbe1b827fad6b88127b052cfa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383478f9e7b471d6bd3a283b0631083f",
+    "url": "/giffr/index.html"
+  },
+  {
+    "revision": "db527acafe820a019065",
+    "url": "/giffr/static/css/2.249dbf59.chunk.css"
+  },
+  {
+    "revision": "33257cf295f2e386172a",
+    "url": "/giffr/static/css/main.4aa0ce29.chunk.css"
+  },
+  {
+    "revision": "db527acafe820a019065",
+    "url": "/giffr/static/js/2.f310ce2c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/giffr/static/js/2.f310ce2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33257cf295f2e386172a",
+    "url": "/giffr/static/js/main.63e8664f.chunk.js"
+  },
+  {
+    "revision": "2d093b2112a3108ef664",
+    "url": "/giffr/static/js/runtime-main.6e3ec957.js"
+  }
+]);
